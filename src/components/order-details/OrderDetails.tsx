4f007c90f3a023bad497b9a0420a4128ad533009
krylovis <kis.226@yandex.ru>
2025-07-19 15:34:15 +0300

    add OrderDetails css

--- src/components/order-details/OrderDetails.tsx
+++ src/components/order-details/OrderDetails.tsx
@@ -1,15 +1,31 @@
+import classNames from 'classnames';
 import style from './OrderDetails.module.scss';
 import { Modal } from '../index';
+import checkGraphics from '../../images/check-graphics.svg';
 
 interface IProps {
   closeModal: () => void,
 }
 
 export default function OrderDetails({ closeModal }: IProps) {
+  const orderNumberClass = classNames(style.text, style.orderNumber);
+  const idTextClass = classNames(style.text, style.orderId);
+  const secondaryTextClass = classNames(style.text, style.textSecondary);
+
   return (
     <Modal closeModal={closeModal}>
       <div className={style.container}>
-        {/* */}
+        <p className={orderNumberClass}>034536</p>
+        <p className={idTextClass}>идентификатор заказа</p>
+
+        <img
+          className={style.image}
+          src={checkGraphics as string}
+          alt="img"
+        />
+
+        <p className={style.text}>Ваш заказ начали готовить</p>
+        <p className={secondaryTextClass}>Дождитесь готовности на орбитальной станции</p>
       </div>
     </Modal>
   );
