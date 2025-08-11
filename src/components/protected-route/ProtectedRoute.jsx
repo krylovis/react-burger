7ab06e7f199aae14262e1ed65e1dcb523788fa3a
krylovis <kis.226@yandex.ru>
2025-08-11 10:38:50 +0300

    fix ProtectedRoute

--- src/components/protected-route/ProtectedRoute.jsx
+++ src/components/protected-route/ProtectedRoute.jsx
@@ -1,8 +1,22 @@
 import { Navigate } from 'react-router-dom';
 import { useAppSelector } from '../../services/store';
 import { selectIsAuth } from '../../services/store/slices/auth/auth.slice';
+import { useLocation } from 'react-router-dom';
+import { ROUTES } from '../../utils/constants';
 
-export default function ProtectedRoute({ element: Component, ...props }) {
+export default function ProtectedRoute({ element: Component, anonymous = false, ...props }, ) {
   const isAuth = useAppSelector(selectIsAuth);
-  return isAuth ? <Component {...props}/> : <Navigate to="/login" replace/>;
+  
+  const location = useLocation();
+  const from = location.state?.from || '/';
+
+  if (anonymous && isAuth) {
+    return <Navigate to={ from } />;
+  }
+
+  if (!anonymous && !isAuth) {
+    return <Navigate to={ROUTES.LOGIN} state={{ from: location}}/>;
+  }
+
+  return <Component {...props}/>;
 }
