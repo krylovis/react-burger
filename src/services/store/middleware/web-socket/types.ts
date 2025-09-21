a8f94fdd13db572facf015a6a000ced7af4318cf
krylovis <kis.226@yandex.ru>
2025-09-21 19:25:54 +0300

    add types

---
+++ src/services/store/middleware/web-socket/types.ts
@@ -0,0 +1,35 @@
+export const WS_CONNECTION_SUCCESS: 'WS_CONNECTION_SUCCESS' = 'WS_CONNECTION_SUCCESS';
+export const WS_CONNECTION_START: 'WS_CONNECTION_START' = 'WS_CONNECTION_START';
+export const WS_SEND_MESSAGE: 'WS_SEND_MESSAGE' = 'WS_SEND_MESSAGE';
+export const WS_GET_MESSAGE: 'WS_GET_MESSAGE' = 'WS_GET_MESSAGE';
+export const WS_CONNECTION_ERROR: 'WS_CONNECTION_ERROR' = 'WS_CONNECTION_ERROR';
+export const WS_CONNECTION_CLOSED: 'WS_CONNECTION_CLOSED' = 'WS_CONNECTION_CLOSED';
+
+export interface IWSConnectionStart {
+  readonly type: typeof WS_CONNECTION_START;
+}
+
+export interface IWSConnectionSuccessAction {
+  readonly type: typeof WS_CONNECTION_SUCCESS;
+}
+
+export interface IWSConnectionErrorAction {
+  readonly type: typeof WS_CONNECTION_ERROR;
+  readonly payload: Event;
+}
+
+export interface IWSConnectionClosedAction {
+  readonly type: typeof WS_CONNECTION_CLOSED;
+}
+
+export interface IWSGetMessageAction {
+  readonly type: typeof WS_GET_MESSAGE;
+  readonly payload: any;
+}
+
+export type TWSActions =
+  | IWSConnectionStart
+  | IWSConnectionSuccessAction
+  | IWSConnectionErrorAction
+  | IWSConnectionClosedAction
+  | IWSGetMessageAction;
