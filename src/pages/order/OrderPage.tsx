483f8eeb0d3d3ab50cb30947d754b6661b53467d
krylovis <kis.226@yandex.ru>
2025-10-02 13:16:20 +0300

    fix OrderPage

--- src/pages/order/OrderPage.tsx
+++ src/pages/order/OrderPage.tsx
@@ -1,13 +1,25 @@
 import style from './OrderPage.module.scss';
-import { memo } from 'react';
+import { useEffect, memo } from 'react';
 import { useParams } from 'react-router-dom';
 import { OrderInfo } from '../../components';
-import { useAppSelector } from '../../services/store';
+import { useAppDispath, useAppSelector } from '../../services/store';
 import { selectOrderById } from '../../services/store/slices/orders/orders.slice';
+import { WS_CONNECTION_START, WS_CONNECTION_CLOSED } from '../../services/store/middleware/web-socket/types';
+import { WS_ORDERS_URL } from '../../utils/constants';
+import { getCookie } from '../../utils/cookies';
 
 function OrderPage() {
   const { id } = useParams();
+  const dispatch = useAppDispath();
   const order = useAppSelector((state) => selectOrderById(state, id));
+
+  useEffect(() => {
+    const accessToken = getCookie('accessToken');
+    dispatch({ type: WS_CONNECTION_START, payload: `${WS_ORDERS_URL}?token=${accessToken?.replace('Bearer ', '')}` });
+    return () => {
+      dispatch({ type: WS_CONNECTION_CLOSED });
+    };
+  }, [dispatch]);
 
   return (
     <div className={style.orderPage}>
