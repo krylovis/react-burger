2012b2581fae388066d3ea92ddd817693c9910bc
krylovis <kis.226@yandex.ru>
2025-10-03 10:37:46 +0300

    fix OrderPage

--- src/pages/order/OrderPage.tsx
+++ src/pages/order/OrderPage.tsx
@@ -1,21 +1,26 @@
 import style from './OrderPage.module.scss';
 import { useEffect, memo } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 import { OrderInfo } from '../../components';
 import { useAppDispath, useAppSelector } from '../../services/store';
 import { selectOrderById } from '../../services/store/slices/orders/orders.slice';
 import { WS_CONNECTION_START, WS_CONNECTION_CLOSED } from '../../services/store/middleware/web-socket/types';
-import { WS_ORDERS_URL } from '../../utils/constants';
+import { WS_ORDERS_URL, WS_ALL_ORDERS_URL } from '../../utils/constants';
 import { getCookie } from '../../utils/cookies';
 
 function OrderPage() {
   const { id } = useParams();
+  const location = useLocation();
   const dispatch = useAppDispath();
+
+  const isToken = location.pathname.includes('orders');
   const order = useAppSelector((state) => selectOrderById(state, id));
 
   useEffect(() => {
     const accessToken = getCookie('accessToken');
-    dispatch({ type: WS_CONNECTION_START, payload: `${WS_ORDERS_URL}?token=${accessToken?.replace('Bearer ', '')}` });
+    const url = isToken ? `${WS_ORDERS_URL}?token=${accessToken?.replace('Bearer ', '')}` : WS_ALL_ORDERS_URL;
+
+    dispatch({ type: WS_CONNECTION_START, payload: url });
     return () => {
       dispatch({ type: WS_CONNECTION_CLOSED });
     };
