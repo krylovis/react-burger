a8ccb4e907e85ae16968763e8e22a2e0a21fbafc
krylovis <kis.226@yandex.ru>
2025-10-06 17:09:23 +0300

    add user slice tests

--- src/services/store/slices/auth/auth.test.ts
+++ src/services/store/slices/auth/auth.test.ts
@@ -1,7 +1,26 @@
 import authReducer, { initialAuthState } from './auth.slice';
 
+const testUser = {
+  user: {
+    email: 'email',
+    name: 'name'
+  },
+  isAuth: true,
+  isLoading: false,
+}
+
 describe('auth slice test', () => {
-  it('Начальное состояние хранилища', () => {
+  it('initial auth state', () => {
     expect(authReducer(undefined, { type: '' })).toEqual(initialAuthState);
   });
+
+  it('set user', () => {
+    expect(authReducer(testUser, { type: 'setUser', ...testUser }))
+      .toEqual(testUser)
+  })
+
+  it('logout user', () => {
+    expect(authReducer(initialAuthState, { type: 'logoutUser' }))
+      .toEqual(initialAuthState)
+  })
 });
