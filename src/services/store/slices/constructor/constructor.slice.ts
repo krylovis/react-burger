c1c76640f69c483ce25af0481fca07e69522bf67
krylovis <kis.226@yandex.ru>
2025-10-07 10:19:41 +0300

    add reducers tests

--- src/services/store/slices/constructor/constructor.slice.ts
+++ src/services/store/slices/constructor/constructor.slice.ts
@@ -2,7 +2,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { v4 as uuidv4 } from 'uuid';
 import { TIngredient, TOrderIngredient } from '../types';
 
-interface IConstructorState {
+export interface IConstructorState {
   bun: TIngredient | null,
   orderIngredients: TOrderIngredient[],
   orderNumber: number | null,
